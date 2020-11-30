49be4afef8019dde786a9b04b3041952fe8836c1
AlexViu <viu.alex771@gmail.com>
2020-11-30 13:44:30 +0100

    ejercicio

---
+++ EjerciciosJS/Exercise1Promises.js
@@ -0,0 +1,10 @@
+let data = [
+{name: "Nacho", telephone: "966112233", age: 40},
+{name: "Ana", telephone: "911223344", age: 35},
+{name: "Mario", phone: "611998877", age: 15},
+{name: "Laura", telephone: "633663366", age: 17}
+];
+newPerson ({name: "Juan", phone: "965661564", age: 60});
+newPerson ({name: "Rodolfo", telephone: "910011001", age: 20});
+deletePerson ("910011001");
+console.log (data);
